c96a87f1e0c11ede2bcf5fca46693fe957c765ec
Tommy Works <93413471+7ommyBoy@users.noreply.github.com>
2023-01-07 23:10:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -17,21 +17,51 @@ const person1 = new Person("Emily", "Firefighter")
 const button1 = document.getElementById('button1')
 button1.addEventListener('click', () => {
 
-  console.log(`You clicked me so i shall reveal myself, ${person1.getName()}, ${person1.getJob()}`)
+  console.log(`Banannas!`)
+
+  // console.log(`You clicked me so i shall reveal myself, ${person1.getName()}, ${person1.getJob()}`)
 
 })
 
 const button2 = document.getElementById('button2')
 button2.addEventListener('click', () => {
+  console.log(`Orange!`)
 
-  console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
+  // console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
 
 })
 
 const button3 = document.getElementById('button3')
 button3.addEventListener('click', () => {
+  console.log(`Mango!`)
 
-  console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
+  // console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
+
+})
+
+const button4 = document.getElementById('button4')
+button4.addEventListener('click', () => {
+  console.log(`Banannas!`)
+
+  // console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
+
+})
+
+const button5 = document.getElementById('button5')
+button5.addEventListener('click', () => {
+  console.log(`Orange!`)
+
+  // console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
+
+})
+
+const button6 = document.getElementById('button6')
+button6.addEventListener('click', () => {
+  console.log(`Mango!`)
+
+  // console.log(`You clicked me so i shall reveal myself, ${person1.getName(), person1.getJob()}`)
+
+})
 
 })
 
